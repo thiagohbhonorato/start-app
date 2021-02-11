0699654d088f371e4955572e8eec32fcb621d10e
Thiago Honorato <thiagohbhonorato@gmail.com>
2021-02-11 12:10:00 -0300

    Primeira organização do prjeto

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
-import styles from "../styles/Start.module.css";
+import styles from "../../styles/Settings.module.css";
 import Link from "next/link";
 
-export default function Home() {
+export default function Settings() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Start.app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Link href="/settings">
-        <a>Settings</a>
+      <Link href="/">
+        <a>Start</a>
       </Link>
     </div>
   );
